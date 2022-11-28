e12b0653de8c4c30105db37ed81280118f908390
ealderson21 <50850846+ealderson21@users.noreply.github.com>
2022-11-27 20:17:54 -0500

    Updated event.py Constructor to typecheck

--- Model/event.py
+++ Model/event.py
@@ -5,7 +5,6 @@ class Event:
     
 
     # events will be determined either a meeting or a task, then the title will be given, then description, then date, then time of day
-
     def __init__(self, title, description, datetime):
 
         if (type(title) != str):
