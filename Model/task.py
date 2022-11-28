76f5a79232b2774e97c93d01f078b153ac2224f0
ealderson21 <50850846+ealderson21@users.noreply.github.com>
2022-11-27 20:16:18 -0500

    Commented task.py constructor

--- Model/task.py
+++ Model/task.py
@@ -1,7 +1,7 @@
 
 from event import Event
 
-# may need to rework the priority system
+# may need to rework the priority system, !!! or number system?
 class Task(Event):
     def __init__(self, title, description, datetime, priority):
         super().__init__(title, description, datetime)
