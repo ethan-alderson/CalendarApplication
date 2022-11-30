e69e35ff463b3e2ed8c193e2593403c4cf30a4b8
ealderson21 <50850846+ealderson21@users.noreply.github.com>
2022-11-30 09:59:18 -0500

    Updated time handling for event.py

--- Model/event.py
+++ Model/event.py
@@ -1,11 +1,11 @@
 
-from datetime import date, time, datetime
+from datetime import time
 
 class Event:
     
 
     # events will be determined either a meeting or a task, then the title will be given, then description, then date, then time of day
-    def __init__(self, title, description, datetime):
+    def __init__(self, title, description, startHour, startMinute, endHour, endMinute):
 
         if (type(title) != str):
             raise TypeError("Invalid input for title.")
@@ -13,11 +13,10 @@ class Event:
         if (type(description) != str):
             raise TypeError("Invalid input for description.")
 
-        if (type(datetime) != datetime):
-            raise TypeError("Invalid input for event's datetime.")
-
-
+            
         self.title = title
         self.description = description
-        self.datetime = datetime
+        self.startTime = time(startHour, startMinute, 0)
+        self.endTime = time(endHour, endMinute, 0)
+
 
