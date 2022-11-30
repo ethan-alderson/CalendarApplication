e1691777817cbd76e4cf96a97748bb69c1430db1
ealderson21 <50850846+ealderson21@users.noreply.github.com>
2022-11-30 12:32:27 -0500

    Reworked Calendar structure to operate over multiple years

--- Model/calendar.py
+++ Model/calendar.py
@@ -4,7 +4,11 @@ from year import Year
 # represents a calendar
 class Calendar:
     
-    def __init__(self, year):
-        
-        if (type(year) != year):
-            raise TypeError("Calendar argument must be a year.")
+    def __init__(self):
+        self.years = {}
+
+    
+    def addYear(self, year):
+        self.years[year.calendarYear] = year
+
+
