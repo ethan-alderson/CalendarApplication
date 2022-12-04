b5a9c657cd550decada4265c69af271a170fdacb
ealderson21 <50850846+ealderson21@users.noreply.github.com>
2022-12-03 23:20:59 -0500

    removed meeting.py type checker

--- Model/meeting.py
+++ Model/meeting.py
@@ -6,8 +6,5 @@ class Meeting(Event):
     def __init__(self, title, description, datetime, recipient):
         super().__init__(title, description, datetime)
 
-        if (type(recipient) != str):
-            raise TypeError("Invalid input for recipient.")
-
         # could introduce a recipient class?
         self.recipient = recipient
