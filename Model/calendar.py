c24c69bc4b50d2bf09428318b0e2740e9dfa3650
Ethan <ethanalderson777@gmail.com>
2022-12-09 00:46:21 -0500

    now initializes from 2000-2099 instead of 2098

--- Model/calendar.py
+++ Model/calendar.py
@@ -7,6 +7,6 @@ class Calendar:
     def __init__(self, name):
         self.years = []
 
-        for i in range(99):
+        for i in range(100):
             self.years.append(Year(2000 + i, (2000 + i) % 4))
     
