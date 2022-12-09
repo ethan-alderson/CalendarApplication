4255183e36669de82c6e10dcf27a2a0d67cda27b
Ethan <ethanalderson777@gmail.com>
2022-12-08 23:45:07 -0500

    implemented user interaction handler in year_view

--- View/year_view.py
+++ View/year_view.py
@@ -2,6 +2,7 @@
 from .month_view import MonthView
 from Model.year import Year
 from Model.calendar import Calendar
+from View.calendar_view import CalendarView
 
 class YearView:
     
@@ -20,7 +21,37 @@ class YearView:
         
 
     def handle_year_options(self, parentCalendar: Calendar):
-        print(f'Date: {self.day.date}')
-        print(f'a) Add Event')
-        print(f'b) Remove Event')
-        print(f'c) View {parentCalendar.name}.')
+        
+        viewingYear = True
+        
+        while (viewingYear):
+                
+            print(f'Year: {self.year.calendarYear}')
+            print(f'a) View a month')
+            print(f'b) View {parentCalendar.name}.')
+        
+            option = input('Select an option (a, b, c): ')
+        
+            if option != 'a' and option != 'b' and option != 'c':
+                print('Invalid option, select again.')
+                continue
+            
+            if option == 'a':
+                
+                gettingPosition = True
+                while (gettingPosition):
+                    position = int(input(f'Which month would you like to view (enter the position in the year, starting at 1): '))
+                
+                if position < 0:
+                    print("Position cannot be negative.")
+                    
+                if position > 12:
+                    print("Position out of bounds.")
+                
+                monthViewer = MonthView(self.year.months[position - 1])
+                monthViewer.display()
+                
+            if option == 'b':
+                
+                calendarViewer = CalendarView(parentCalendar)
+                calendarViewer.display()
