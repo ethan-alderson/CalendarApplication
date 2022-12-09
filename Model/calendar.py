3f45ec3ca057ea27bcf86b991f21158f40de5c49
Ethan <ethanalderson777@gmail.com>
2022-12-08 23:55:08 -0500

    Calendar now fills with empty years in constructor

--- Model/calendar.py
+++ Model/calendar.py
@@ -7,6 +7,8 @@ class Calendar:
     def __init__(self, name):
         self.years = {}
 
+        for i in range(99):
+            self.years[2000 + i] = Year(2000 + i, (2000 + i) % 4)
     
     def addYear(self, year):
         self.years[year.calendarYear] = year
