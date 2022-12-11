2d1454a7344241168cf4416e5388e9477508ea5d
Ethan <ethanalderson777@gmail.com>
2022-12-10 19:32:48 -0500

    Implemented sorted insertion into day.events

--- Model/day.py
+++ Model/day.py
@@ -1,5 +1,5 @@
 from .event import Event
-from datetime import date
+from datetime import date, timedelta
 
 class Day:
     def __init__(self, year, month, day):
@@ -15,7 +15,29 @@ class Day:
             newEvent (Event): the event to be added
         """
         
-        self.events.append(newEvent)
+        added = False
+
+        if len(self.events) == 0:
+            self.events.append(newEvent)
+            added = True
+
+        else:
+            # for each event in the range
+
+            low = 0
+            high = None
+
+            if low < 0:
+                raise ValueError('lo must be non-negative')
+            if high is None:
+                high = len(self.events)
+            while low < high:
+                middle = (low + high) // 2
+                if newEvent.startTime.hour < self.events[middle].startTime.hour:
+                    high = middle
+                else:
+                    low = middle + 1
+            self.events.insert(low, newEvent)
 
 
 
