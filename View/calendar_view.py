ddd119a82dc9696e877666b9ebef68798f363d00
Ethan <ethanalderson777@gmail.com>
2022-12-12 22:15:42 -0500

    cleaned calendar_view

--- View/calendar_view.py
+++ View/calendar_view.py
@@ -6,15 +6,29 @@ import sys
 import io
 
 class CalendarView:
-
+    """represents the UI for the calendar
+    """
     def __init__(self, calendar: Calendar) -> None:
         self.calendar = calendar
     
     def display(self):
+        """Displays the initial method
+
+        VOID
+        """
         print(f'Welcome to your Calendar! This is a calendar of your events from {self.calendar.years[0].calendarYear} to {self.calendar.years[-1].calendarYear}!')
     
     @staticmethod
     def testDisplay(calendar: Calendar):
+        """ test method, all it does is run display but reroute the print statement into a StringIO object. 
+        It does this so that we can check the display output in our tests even though it only prints to the console.
+
+        Args:
+            calendar (Calendar): the calendar to test display
+
+        Returns:
+            String: The full displayed string of the calendar
+        """
         capturedOutput = io.StringIO()
         sys.stdout = capturedOutput
         cv1 = CalendarView(calendar)
@@ -24,12 +38,18 @@ class CalendarView:
         return capturedOutput.getvalue()
 
     def handle_calendar_options(self):
+        """Handles user inputs in the calendar menu
+
+        VOID
+        """
+
         viewingCalendar = True
         
         while(viewingCalendar):
             
             self.display()
             
+            # print initial options
             print(f'Name: {self.calendar.name}')
             print(f'a) Select a Year')
             print(f'b) Exit Program.')
@@ -40,6 +60,7 @@ class CalendarView:
                 print('Invalid option, select again.')
                 continue
             
+            # take the year input and open up that display
             if option == 'a':
                 
                 gettingYear = True
@@ -58,6 +79,7 @@ class CalendarView:
                 yearViewer = YearView(yearToView)
                 yearViewer.handle_year_options(self.calendar)   
             
+            # save the progress on this calendar
             if option == 'b':
                 viewingCalendar = False
 
